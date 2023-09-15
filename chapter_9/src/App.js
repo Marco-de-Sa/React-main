5bf74e1dfab4f135bbbb8f1015b28ad05e12a8a2
Marco-de-Sa <marcojb.desa@gmail.com>
2023-09-15 12:26:53 +0200

    react chapter 9

--- chapter_9/src/App.js
+++ chapter_9/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/app';
+import "firebase/compat/database";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserForm from './UserForm';
+import User from './User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    console.log(firebase);
+  }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Switch>
+            <Route path="/edit/:id" component={UserForm} /> 
+              <Route path="/add" component={UserForm} />
+              <Route exact path="/" component={User} />
+              <Route path="/*" component={NotFound} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+class NotFound extends Component {
+  render() {
+    return <div>Not Found</div>
+  }
+}
--- chapter_9/src/index.js
+++ chapter_9/src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+var config = {
+  apiKey: "AIzaSyAhFiLFSBHJXJ2uIJHOveUAoIVEUD_uSGg",
+  authDomain: "chapter9-4eb39.firebaseapp.com",
+  projectId: "chapter9-4eb39",
+  storageBucket: "chapter9-4eb39.appspot.com",
+  messagingSenderId: "801768744792",
+  appId: "1:801768744792:web:adf11da5842d1388bde72d",
+  measurementId: "G-2H74EWHR71"
+}
+
+firebase.initializeApp(config);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
